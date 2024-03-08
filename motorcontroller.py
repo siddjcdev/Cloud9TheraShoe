1804ad0b02c0aee179fdb7b248bd2a9e093dd9b4
siddjcdev <125834575+siddjcdev@users.noreply.github.com>
2024-03-08 08:55:28 -0500

    Add files via upload
    Added changes to the motor code

--- motorcontroller.py
+++ motorcontroller.py
@@ -1,85 +1,52 @@
-from machine import Pin
-from time import sleep_ms
+from machine import Pin, PWM
+from time import sleep
 import uasyncio as asyncio
 
-led = Pin(2, Pin.OUT)
-motor1 = Pin(32,Pin.OUT)
-motor2 = Pin(33, Pin.OUT)
-motor3 = Pin(27,Pin.OUT)
-motor4 = Pin(12, Pin.OUT)
-motor5 = Pin(13,Pin.OUT)
-motor6 = Pin(10, Pin.OUT)
 
-async def start_motors():
+
+async def intensity(level):
     print("start motors")
-    try:
-        while True:
-            led.value(1)
-            motor1.value(1)
-            motor2.value(1)
-            motor3.value(1)
-            motor4.value(1)
-            motor5.value(1)
-            motor6.value(1)
-            await asyncio.sleep_ms(1000)
-            led.value(0)
-            await asyncio.sleep_ms(1000)
-    except Exception as e:
-        led.value(0)
-        motor1.value(0)
-        motor2.value(0)
-        motor3.value(0)
-        motor4.value(0)
-        motor5.value(0)
-        motor6.value(0)
+    led = Pin("WL_GPIO0", Pin.OUT)
+    led.off()
+
+    # Set up PWM Pin
+    motor1 = Pin(6, Pin.PULL_UP)
+    motor1_pwm = PWM(motor1)
+    motor2 = Pin(10, Pin.PULL_UP)
+    motor2_pwm = PWM(motor2)
+
+
+    duty_cycle = 0  #
+    duty_step = 16384
+
+    #Set PWM frequency
+    frequency = 500
+    motor1_pwm.freq(frequency)
+    motor2_pwm.freq(frequency)
+
+    iteration = 1
+
     
+    if level == 0:
+        duty_cycle = 0
+        print("The motors are off.")
+    else:
+        duty_cycle = duty_step * level
+        print("Intensity level:", level)
+    
+    try:
+      while True:
+        motor1_pwm.duty_u16(duty_cycle)
+        motor2_pwm.duty_u16(duty_cycle)
+ 
+        led.toggle()
+        #print("Toggled led. iteration:", iteration)
+        iteration +=1
+        await asyncio.sleep_ms(1000) 
+            
+    except Exception as e:
+        led.off()
         print ("An error has occurred. Please try again. The error is:")
         print (e)      
 
-        pass    
-    
-
-async def stop_motors():
-    print("stopping motors...")
-    try:
-            led.value(0)
-            motor1.value(0)
-            motor2.value(0)
-            motor3.value(0)
-            motor4.value(0)
-            motor5.value(0)
-            motor6.value(0)
-            await asyncio.sleep_ms(1000)
-    except Exception as e:
-        led.value(0)
-        motor1.value(0)
-        motor2.value(0)
-        motor3.value(0)
-        motor4.value(0)
-        motor5.value(0)
-        motor6.value(0)
-    
-        print ("An error has occurred. Please try again. The error is:")
-        print (e)
-        
-
-        pass    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        pass
